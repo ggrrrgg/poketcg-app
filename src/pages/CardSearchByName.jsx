59aa355aa1e3d7c5719b109b2356c4c76c78f147
ggrrrgg <126563391+ggrrrgg@users.noreply.github.com>
2023-10-07 12:46:13 +1100

    fix unused import

--- src/pages/CardSearchByName.jsx
+++ src/pages/CardSearchByName.jsx
@@ -6,7 +6,7 @@ import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { Card } from "react-bootstrap";
+// import { Card } from "react-bootstrap";
 
 
 export default function CardSearchByName() {
